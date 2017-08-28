705a6416476746874334aeac2471c8a528c1d209
ceb-student <CEB-Student@caddoschools.org>
2017-08-28 08:20:09 -0500

    changed class per.

---
+++ LearningAboutStrings/src/basicInputOutput/controller/BasicIOController.java
@@ -0,0 +1,12 @@
+package basicInputOutput.controller;
+
+public class BasicIOController 
+{
+     public void printGreeting()
+     {
+    	 String firstName = "Joshua";
+    	 String lastName = "Danzell";
+    	 System.out.println(firstName + " " + lastName);
+ 	
+     }
+}
