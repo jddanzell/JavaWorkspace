10038487002bf368516340ca64e9b1905f6a8388
ceb-student <CEB-Student@caddoschools.org>
2017-08-29 08:24:03 -0500

    added classes

---
+++ BetterJPAMs/src/studentManagement/controller/StudentRunner.java
@@ -0,0 +1,12 @@
+package studentManagement.controller;
+
+import studentManagement.model.*;
+
+public class StudentRunner 
+{	
+     public static void main(String[] args)
+     {
+    	  Student myStudent = new Student();
+    	  System.out.println(myStudent);
+     }
+}
